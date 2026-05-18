8b85f4871c0000623fba8551b494229b2a93e2bf
renatastephanie <renatastephaniesantos@gmail.com>
2026-05-18 20:28:30 -0300

    fix: Impotado o AboutVideo no App.tsx

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,3 +1,4 @@
+import { AboutVideo } from "./components/AboutVideo/AboutVideo"
 import { Education } from "./components/Education/Education"
 import { Header } from "./components/Header/Header"
 import { ProfileCard } from "./components/ProfileCard/ProfileCard"
@@ -12,10 +13,11 @@ function App() {
       <main>
         <ProfileCard />
       </main>
-      
+
       <section id="detalhes">
+        <AboutVideo/>
         <Skills />
-        <Education/>
+        <Education />
       </section>
     </>
   )
