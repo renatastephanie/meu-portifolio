81bf5a4f58b68c96630c4bc4b678d6d1869198bb
renatastephanie <renatastephaniesantos@gmail.com>
2026-05-19 18:05:18 -0300

    feat: adicionado uma skill nova

--- front-end/src/components/Skills/Skills.tsx
+++ front-end/src/components/Skills/Skills.tsx
@@ -14,6 +14,7 @@ const minhasSkills: ISkillItem[] = [
   { nome: "React.Js", porcentagem: 60, icone: "fa-brands fa-react" },
   { nome: "Node.Js", porcentagem: 78, icone: "fa-brands fa-node-js" },
   { nome: "Python", porcentagem: 42, icone: "fa-brands fa-python" },
+  { nome: "Inglês", porcentagem: 52, icone: "fa-solid fa-language" },
   {nome: "Trabalho em Equipe",porcentagem: 90,icone: "fa-solid fa-people-group",},
 ];
 
