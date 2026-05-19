df63ac380dfa0304ca178b60c43282ca9db7834d
renatastephanie <renatastephaniesantos@gmail.com>
2026-05-19 18:39:22 -0300

    fix: corrige caminho da foto de prévia dos projetos

--- front-end/src/components/Projects/Projects.tsx
+++ front-end/src/components/Projects/Projects.tsx
@@ -1,5 +1,18 @@
 import styles from "./Projects.module.scss";
 
+// IMPORT DAS IMAGENS DOS PROJETOS
+import todoList from "../../assets/TODO-list-react.png";
+import climaTempo from "../../assets/clima-tempo.png";
+import detonaRalph from "../../assets/detona-ralph.png";
+import geradorSenha from "../../assets/gerador-de-senha.png";
+import dragonBall from "../../assets/dragon-ball.png";
+import cordel from "../../assets/cordel.png";
+import landingReceita from "../../assets/landing-page-receita.png";
+import verificadorIdade from "../../assets/verificador-idade.png";
+import groupAgencia from "../../assets/group-agencia.png";
+import calculadoraChurrasco from "../../assets/calculadora-churrasco.png";
+import starbucks from "../../assets/starbucks.png";
+
 interface IProjectItem {
   ano: string;
   titulo: string;
@@ -16,7 +29,7 @@ const meusProjetos: IProjectItem[] = [
     subtitulo: "Projeto construído para treinar Aplicações em React",
     descricao:
       "Aplicação de gerenciamento de tarefas desenvolvida com uma arquitetura moderna, utilizando React no front-end, Node.js no back-end e persistência robusta em banco de dados PostgreSQL.",
-    imagem: "src/assets/TODO-list-react.png",
+    imagem: todoList,
     repositorio: "https://github.com/renatastephanie/todo-list-react",
   },
 
@@ -26,7 +39,7 @@ const meusProjetos: IProjectItem[] = [
     subtitulo: "Projeto construído para treinar o consumo de APIs",
     descricao:
       "Projeto simples que permite ao usuário consultar o clima atual de uma cidade. Ele exibe a temperatura atual em graus Celsius utilizando a OpenWeatherMap API",
-    imagem: "src/assets/clima-tempo.png",
+    imagem: climaTempo,
     repositorio: "https://github.com/renatastephanie/clima-tempo",
   },
 
@@ -35,7 +48,7 @@ const meusProjetos: IProjectItem[] = [
     titulo: "Jogo Detona Ralph",
     descricao:
       "Este é um jogo simples, no estilo Whac-a-Mole, inspirado no filme Detona Ralph. O objetivo do jogo é clicar nos quadrados onde o Ralph aparece para marcar pontos antes que o tempo acabe.",
-    imagem: "src/assets/detona-ralph.png",
+    imagem: detonaRalph,
     repositorio: "https://github.com/renatastephanie/detona-ralph-jogo",
   },
 
@@ -44,7 +57,7 @@ const meusProjetos: IProjectItem[] = [
     titulo: "Gerador de Senhas",
     descricao:
       "Um gerador de senhas moderno, responsivo e personalizável, feito com HTML, CSS e JavaScript",
-    imagem: "src/assets/gerador-de-senha.png",
+    imagem: geradorSenha,
     repositorio: "https://github.com/renatastephanie/gerador_de_senhas",
   },
 
@@ -53,7 +66,7 @@ const meusProjetos: IProjectItem[] = [
     titulo: "Projeto Dragon Ball",
     descricao:
       "Este projeto é uma reprodução simples de uma página do dragon ball z, feita com HTML, CSS e JavaScript apenas.",
-    imagem: "src/assets/dragon-ball.png",
+    imagem: dragonBall,
     repositorio: "https://github.com/renatastephanie/projeto_dragonball",
   },
 
@@ -61,7 +74,7 @@ const meusProjetos: IProjectItem[] = [
     ano: "2025",
     titulo: "Cordel Moderno",
     descricao: "Projeto feito para treino do efeito paralax",
-    imagem: "src/assets/cordel.png",
+    imagem: cordel,
     repositorio: "https://github.com/renatastephanie/projeto_dragonball",
   },
 
@@ -70,7 +83,7 @@ const meusProjetos: IProjectItem[] = [
     titulo: "Lading Page de Receita",
     descricao:
       "Esta landing page foi desenvolvida como parte de um desafio de front-end, com o objetivo de praticar e aprimorar habilidades em HTML e CSS. O projeto apresenta uma receita de omelete simples, com um layout responsivo e visual agradável, inspirado em designs modernos.",
-    imagem: "src/assets/landing-page-receita.png",
+    imagem: landingReceita,
     repositorio: "https://github.com/renatastephanie/landing_page_receita",
   },
 
@@ -79,7 +92,7 @@ const meusProjetos: IProjectItem[] = [
     titulo: "Verificador de Idade",
     descricao:
       "Um simples projeto em HTML, CSS e JavaScript que permite ao usuário verificar a sua idade com base no ano de nascimento e selecionar o gênero. O resultado é exibido dinamicamente na página.",
-    imagem: "src/assets/verificador-idade.png",
+    imagem: verificadorIdade,
     repositorio: "https://github.com/renatastephanie/verificador_de_idade",
   },
 
@@ -89,7 +102,7 @@ const meusProjetos: IProjectItem[] = [
     subtitulo: "Projeto Educacional do Curso Proz",
     descricao:
       "Projeto educacional do curso Proz feito em grupo com o intuito de aplicar os conhecimentos adquiridos durante o curso.",
-    imagem: "src/assets/group-agencia.png",
+    imagem: groupAgencia,
     repositorio: "https://github.com/renatastephanie/Proz---Grupo-1---TC-38",
   },
 
@@ -99,7 +112,7 @@ const meusProjetos: IProjectItem[] = [
     subtitulo: "Projeto Educacional da Faculdade",
     descricao:
       "Projeto desenvolvido como parte de um trabalho universitário. A Calculadora de Churrasco ajuda a estimar as quantidades necessárias de alimentos e bebidas para um churrasco, com base no número de homens, mulheres e crianças presentes.",
-    imagem: "src/assets/calculadora-churrasco.png",
+    imagem: calculadoraChurrasco,
     repositorio:
       "https://github.com/renatastephanie/calculadora_para_churrasco",
   },
@@ -109,7 +122,7 @@ const meusProjetos: IProjectItem[] = [
     titulo: "Starbucks Landing Page",
     descricao:
       "Este projeto é uma recriação da página inicial do site do Starbucks, com foco na apresentação visual de produtos e uma pequena interação para trocar a imagem e o fundo do copo da Starbucks.",
-    imagem: "src/assets/starbucks.png",
+    imagem: starbucks,
     repositorio: "https://github.com/renatastephanie/projeto_starbucks",
   },
 ];
